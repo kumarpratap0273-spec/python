12a4bdd8510e753b461dfe857837fd6284e5d068
Pratap kumar Singh <kumarpratap0273@gmail.com>
2026-07-11 00:54:18 +0530

    initial commit

---
+++ conversion.py
@@ -0,0 +1,6 @@
+#type conversion
+a=int("2")
+b=3.1
+
+sum=a+b
+print(sum) 
---
+++ data type.py
@@ -0,0 +1,11 @@
+name= "pratap"
+age=23
+price=23.3
+print(name)
+print(age)
+print(price)
+
+
+print(type(name))
+print(type(age))
+print(type(price))
