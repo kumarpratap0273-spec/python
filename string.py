7b2ab616fbb01e627f0ed7d9b013a4a4e314e92a
Pratap kumar Singh <kumarpratap0273@gmail.com>
2026-07-11 23:37:30 +0530

    update python

---
+++ string.py
@@ -0,0 +1,34 @@
+str1="I Am PARATP KUMAR "
+str2="I AM PARATP KUMAR singh .\n i am agood boy "
+print(str1)
+print(str2)
+#concatenation
+print(str1 + str2)
+
+#length of string
+print(len(str1))
+print(len(str2))
+
+#indexing
+print(str1[0])
+print(str2[0]) 
+
+#slicing string
+print(str1[0:3])
+
+#upper case
+print(str2.upper())
+
+#lower case
+print(str1.lower())
+
+#remove whitespace
+print(str1.strip())
+print(str2.strip())
+
+#replace string
+print(str2.replace("singh","SINGH"))
+
+#SPLIT STRING
+print(str2.split(","))
+
