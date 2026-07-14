272d79dfbbf9ec5a6fb4241ce9fe8ac6cd263410
Pratap kumar Singh <kumarpratap0273@gmail.com>
2026-07-15 02:03:52 +0530

    set upadte

--- set.py
+++ set.py
@@ -1,4 +1,28 @@
 # A set in Python is a built-in data type that stores an unordered collection of unique, immutable elements, meaning duplicates are automatically removed and the order of items is not preserved.
 
-myset = {"apple", "banana", "cherry"}
+myset = {"apple",1,2, "banana", "cherry"}
 print(myset)
+print(type(myset))
+print(len(myset))
+
+#set method
+print("adding new element of a set")
+
+#adding new elemnt
+myset.add("orange")
+print(myset)
+
+#update
+myset.update(["grapes","kiwi"])
+print(myset)
+
+#remove
+myset.remove("banana")
+print(myset)
+
+#discard
+myset.discard("kiwi")
+print(myset)
+
+
+
